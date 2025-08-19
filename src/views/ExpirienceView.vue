761566130505f076cb02da45895c4a749971ada5
Leonardo Santos <81315155+Je-Leo-AS@users.noreply.github.com>
2025-08-19 19:33:00 -0300

    fix experiencias

--- src/views/ExpirienceView.vue
+++ src/views/ExpirienceView.vue
@@ -12,35 +12,35 @@
         </div>
         <div class="experience-item">
           <div class="education-text">
-            <h3><a href='https://ufpr.br/' target="_blank">{{ t('experience_pet') }}</a></h3>
+            <h3><a href='https://gics.ufpr.br/' target="_blank">{{ t('experience_gics') }}</a></h3>
             <p>{{ t('experience_pet_description') }}</p>
             <p class="experience-date">10/2022 - {{ t('present') }}</p>      
           </div>
-          <img :src="require('@/assets/images/iotag.webp')" alt="Work Image" class="experience-image">
+          <img :src="require('@/assets/images/GICS.webp')" alt="Work Image" class="experience-image">
         </div>
         <div class="experience-item">
           <div class="education-text">
-            <h3><a href='https://ufpr.br/' target="_blank">{{ t('experience_baja') }}</a></h3>
+            <h3><a href="https://insta.ufpr.br/projetos/baja-sae/" target="_blank">{{ t('experience_baja') }}</a></h3>
             <p>{{ t('experience_baja_description') }}</p>
             <p class="experience-date">10/2022 - {{ t('present') }}</p>      
           </div>
-          <img :src="require('@/assets/images/iotag.webp')" alt="Work Image" class="experience-image">
+          <img :src="require('@/assets/images/BAJA.webp')" alt="Work Image" class="experience-image">
         </div>
         <div class="experience-item">
           <div class="education-text">
-            <h3><a href='https://ufpr.br/' target="_blank">{{ t('experience_gics') }}</a></h3>
+            <h3><a href='https://lactec.com.br/' target="_blank">{{ t('experience_lactec') }}</a></h3>
             <p>{{ t('experience_gics_description') }}</p>
             <p class="experience-date">10/2022 - {{ t('present') }}</p>      
           </div>
-          <img :src="require('@/assets/images/iotag.webp')" alt="Work Image" class="experience-image">
+          <img :src="require('@/assets/images/Lactec.webp')" alt="Work Image" class="experience-image">
         </div>
         <div class="experience-item">
           <div class="education-text">
-            <h3><a href='https://ufpr.br/' target="_blank">{{ t('experience_lactec') }}</a></h3>
+            <h3><a href='https://www.instagram.com/pet_eletrica/' target="_blank">{{ t('experience_PET') }}</a></h3>
             <p>{{ t('experience_lactec_description') }}</p>
             <p class="experience-date">10/2022 - {{ t('present') }}</p>      
           </div>
-          <img :src="require('@/assets/images/iotag.webp')" alt="Work Image" class="experience-image">
+          <img :src="require('@/assets/images/PET.webp')" alt="Work Image" class="experience-image">
         </div>
       </div>
     </section>
